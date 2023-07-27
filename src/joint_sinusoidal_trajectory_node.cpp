c65b18cffa014cdfee2598c6477e4513a19bea4d
Lukasz Wolinski <lukasz.wolinski@pw.edu.pl>
2023-07-27 10:54:45 +0200

    It still compiles!

--- src/joint_sinusoidal_trajectory_node.cpp
+++ src/joint_sinusoidal_trajectory_node.cpp
@@ -12,11 +12,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "trajectory_msgs/msg/joint_trajectory_point.hpp"
-//~ #include "std_msgs/msg/float64.hpp" //TODO: temporary, because deprecated? create my own "local_time" message?
-
-// TODO: Create an action server in this node. Other node sends a request with the start and goal point, and maximum vel and acc.
-// In the for loop, the trajectory is computed and published (the function TopicCallback should be repurposed).
-// https://docs.ros.org/en/humble/Tutorials/Intermediate/Writing-an-Action-Server-Client/Cpp.html
 
 class JointSinusoidalTrajectoryNode : public rclcpp::Node
 {
@@ -24,12 +19,8 @@ public:
     using PTP = rrlib_interfaces::action::PTP;
     using GoalHandlePTP = rclcpp_action::ServerGoalHandle<PTP>;
     JointSinusoidalTrajectoryNode();
-    //~ void SetParameters( const Eigen::VectorXd &q_start, const Eigen::VectorXd &q_end, size_t DOF, double vel_max, double acc_max );
-    //~ void SetParametersTest();
     
 private:
-    //~ void TopicCallback(const std_msgs::msg::Float64 & msg);
-    //~ rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_;
     rclcpp::Publisher<trajectory_msgs::msg::JointTrajectoryPoint>::SharedPtr publisher_;
     rclcpp_action::Server<PTP>::SharedPtr action_server_;
     
@@ -54,8 +45,6 @@ JointSinusoidalTrajectoryNode::JointSinusoidalTrajectoryNode()
     
     RCLCPP_INFO(this->get_logger(), "Starting the node.");
     
-    // create a subscriber for the local time // TODO: remove
-    //~ subscription_ = this->create_subscription<std_msgs::msg::Float64>("jnt_sin_local_time", 10, std::bind(&JointSinusoidalTrajectoryNode::TopicCallback, this, _1));
     // create a publisher for the joint trajectory point
     publisher_ = this->create_publisher<trajectory_msgs::msg::JointTrajectoryPoint>("jnt_sin_traj", 10);
     
@@ -67,31 +56,6 @@ JointSinusoidalTrajectoryNode::JointSinusoidalTrajectoryNode()
         std::bind(&JointSinusoidalTrajectoryNode::HandleCancel, this, _1),
         std::bind(&JointSinusoidalTrajectoryNode::HandleAccepted, this, _1));
 }
-
-//~ void JointSinusoidalTrajectoryNode::SetParameters( const Eigen::VectorXd &q_start, const Eigen::VectorXd &q_end, size_t DOF, double vel_max, double acc_max )
-//~ {
-    //~ if (q_start.size() != DOF or q_end.size() != DOF or vel_max <= 0.0 or acc_max <= 0.0)
-    //~ {
-        //~ std::string message;
-        //~ message = "\nParameters are not set. Please, provide the correct values:\n-> q_start and q_end need to be of the same size,\n-> v_max has to be greater than 0,\n-> a max has to be greater than 0.\n";
-        //~ std::cout << message;
-        //~ return;
-    //~ }
-    
-    //~ trajectory_.SetParameters(q_start, q_end, DOF, vel_max, acc_max);
-//~ }
-
-//~ void JointSinusoidalTrajectoryNode::SetParametersTest()
-//~ {
-    //~ /* test function to set the joint trajectory generator parameters */
-    //~ Eigen::VectorXd q_start = Eigen::VectorXd::Zero(7);
-    //~ Eigen::VectorXd q_end(7);
-    //~ q_end << 0.0, 0.0, 0.0, M_PI/2.0, 0.0, -M_PI/2.0, 0.0;
-    //~ size_t DOF = 7;
-    //~ double vel_max = 100.0 / 180.0 * M_PI;
-    //~ double acc_max = 500.0 / 180.0 * M_PI;
-    //~ trajectory_.SetParameters(q_start, q_end, DOF, vel_max, acc_max);
-//~ }
 
 rclcpp_action::GoalResponse JointSinusoidalTrajectoryNode::HandleGoal(
     const rclcpp_action::GoalUUID & uuid,
@@ -195,7 +159,7 @@ void JointSinusoidalTrajectoryNode::Execute(const std::shared_ptr<GoalHandlePTP>
         // Publish feedback
         feedback->percent_complete = t / trajectory_.GetMotionTime() * 100.0;
         goal_handle->publish_feedback(feedback);
-        //~ RCLCPP_INFO(this->get_logger(), "Publish feedback.");
+        RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 1000, "Trajectory percent complete: %lf", feedback->percent_complete);
         
         t += dt;
 
@@ -211,28 +175,6 @@ void JointSinusoidalTrajectoryNode::Execute(const std::shared_ptr<GoalHandlePTP>
         RCLCPP_INFO(this->get_logger(), "Goal succeeded.");
     }
 }
-
-//~ void JointSinusoidalTrajectoryNode::TopicCallback(const std_msgs::msg::Float64 & msg)
-//~ {
-    //~ // given the local time t from <0, end_time>, compute the joint positions, velocities, and accelerations (the trajectory point):
-    //~ double t = msg.data;
-    //~ Eigen::VectorXd q;
-    //~ Eigen::VectorXd dqdt;
-    //~ Eigen::VectorXd d2qdt2;
-    //~ trajectory_.PositionVelocityAcceleration(t, &q, &dqdt, &d2qdt2);
-    
-    //~ // publish the computed joint trajectory point
-    //~ trajectory_msgs::msg::JointTrajectoryPoint response;
-    //~ std::vector<double> positions(q.data(), q.data() + q.size());
-    //~ response.positions = positions;
-    //~ std::vector<double> velocities(dqdt.data(), dqdt.data() + dqdt.size());
-    //~ response.velocities = velocities;
-    //~ std::vector<double> accelerations(d2qdt2.data(), d2qdt2.data() + d2qdt2.size());
-    //~ response.accelerations = accelerations;
-    //~ response.time_from_start.sec = (int) floor(t);
-    //~ response.time_from_start.nanosec = (int) ( (t - floor(t)) * pow(10.0, 9.0) ); // TODO: more legit way?
-    //~ publisher_->publish(response);
-//~ }
 
 int main(int argc, char * argv[])
 {
